d5b0af00c9dd9c7618b9e220973003885d306dd6
Akshay Lahudkar <imakshaylahudkar@gmail.com>
2021-03-11 23:05:56 +0530

    URLify Implementation

--- URLify/main.cpp
+++ URLify/main.cpp
@@ -6,9 +6,48 @@
 //
 
 #include <iostream>
+#include<string>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // insert code hereajaa...
+    std::string str="Mr John Smith    ";
+    
+   // std::cin.ignore();
+    //getline(std::cin,str,'\n');
+    
+    
+    int flag=0;
+    int i=str.size()-1;
+    while(i>=0)
+    {
+        if(flag==0 && str[i]!=' ')
+        {
+            flag=1;
+            i--;
+            
+            
+        }
+        
+        if(flag==1 && str[i]==' ')
+        {
+            //replace i with 3 characters
+            //std::cout<<i<<" ";
+            str.replace(i,1,"%20");
+            i--;
+            
+        }
+       else
+        i--;
+        
+        
+    }
+    
+    
+        
+
+    std::cout<<str<<"\n";
+        
+
+
     return 0;
 }
